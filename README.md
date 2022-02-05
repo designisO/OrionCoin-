8444f8e46b0215f8243dac3d84d9ab169cc78fcd
DesignisO <DesignIsO@protonmail.com>
2022-02-04 19:10:06 -0500

    adding files

---
+++ README.md
@@ -0,0 +1,5 @@
+# OrionCoin "ORC" ERC20 Token
+
+This project demonstrates a ERC20 build created called "OrionCoin". The following was learned while working on this project.
+
+- 
---
+++ hardhat.config.js
@@ -0,0 +1,18 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    ropsten: {
+      url: 'ALchemy API',
+      accounts:['metamask private key']
+    }
+  }
+};
